dce97e4f93d6d12791f52fc7ab5203dcdf83ddb9
Darkmus <darkmus@tut.by>
2018-05-02 14:08:46 +0300

    Added switching for slider (mobile)

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,10 @@
 var button = document.querySelector('.page-header__button');
 var menu = document.querySelector('.main-nav');
+var beforeBtn = document.querySelector('.slider__toggle--before');
+var afterBtn = document.querySelector('.slider__toggle--after');
+var beforeSlide = document.querySelector('.slider__item--before');
+var afterSlide = document.querySelector('.slider__item--after');
+var indicator = document.querySelector('.slider__indicator-green');
 
 button.classList.remove('page-header__button--nojs');
 menu.classList.remove('main-nav--nojs');
@@ -13,4 +18,18 @@ button.addEventListener('click', function(evt) {
     button.classList.add('page-header__button--opened');
     menu.classList.add('main-nav--opened');
   }
+})
+
+beforeBtn.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  afterSlide.classList.remove('slider__item--active');
+  beforeSlide.classList.add('slider__item--active');
+  indicator.classList.add('slider__indicator-green--left');
+})
+
+afterBtn.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  beforeSlide.classList.remove('slider__item--active');
+  afterSlide.classList.add('slider__item--active');
+  indicator.classList.remove('slider__indicator-green--left');
 })
